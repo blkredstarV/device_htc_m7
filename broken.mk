2d04c0e6f9b757a172e9f3002d7a6976680a8897
blkredstarV <rlcg55@gmail.com>
2015-01-10 18:52:50 -0600

    broke it

--- broken.mk
+++ broken.mk
@@ -1,10 +1,10 @@
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+$(call inherit-product, vendor/broken/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/htc/m7/full_m7.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=KTU84L.H4 BUILD_FINGERPRINT="htc/m7_google/m7:4.4.3/KTU84L.H4/347097:user/release-keys" PRIVATE_BUILD_DESC="4.07.1700.4 CL347097 release-keys"
 
-PRODUCT_NAME := slim_m7
+PRODUCT_NAME := broken_m7
 PRODUCT_DEVICE := m7
